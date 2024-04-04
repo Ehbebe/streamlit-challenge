1fea8f8773605c18237135cc6a99dc135865f514
Ehbebe <study2hojin@gmail.com>
2024-04-05 03:41:40 +0900

    update

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 import streamlit as st
+import os
+import re
+
 from langchain.chat_models import ChatOpenAI
 from langchain.document_loaders import UnstructuredFileLoader
 from langchain.text_splitter import CharacterTextSplitter
@@ -8,10 +11,7 @@ from langchain.storage import LocalFileStore
 from langchain.prompts import ChatPromptTemplate
 from langchain.schema.runnable import RunnableLambda, RunnablePassthrough
 from langchain.callbacks.base import BaseCallbackHandler
-import os
 
-if "api_key" not in st.session_state:
-    st.session_state["api_key"] = None
 
 st.set_page_config(
     page_title="DocumentGPT",
@@ -26,6 +26,8 @@ if "api_key" not in st.session_state:
 
 if "api_key_bool" not in st.session_state:
     st.session_state["api_key_bool"] = False
+
+pattern = r"sk-.*"
 
 
 class ChatCallbackHandler(BaseCallbackHandler):
@@ -45,67 +47,11 @@ class ChatCallbackHandler(BaseCallbackHandler):
 llm = ChatOpenAI(
     temperature=0.1,
     streaming=True,
-    callbacks=[
+    callbacks={
         ChatCallbackHandler(),
-    ],
+    },
     openai_api_key=st.session_state["api_key"],
 )
-
-
-@st.cache_data(show_spinner="Embedding file...")
-def embed_file(file):
-    file_content = file.read()
-    file_path = f"./.cache/files/{file.name}"
-    if not os.path.exists(f"./.cache/files/"):
-        os.makedirs(f"./..cache/files/", exist_ok=True)
-
-    with open(file_path, "wb") as f:
-        f.write(file_content)
-
-    os.makedirs(f"./.cache/embeddings/{file.name}", exist_ok=True)
-
-    cache_dir = LocalFileStore(f"./.cache/embeddings/{file.name}")
-    splitter = CharacterTextSplitter.from_tiktoken_encoder(
-        separator="\n",
-        chunk_size=600,
-        chunk_overlap=100,
-    )
-    loader = UnstructuredFileLoader(file_path)
-    docs = loader.load_and_split(text_splitter=splitter)
-    embeddings = OpenAIEmbeddings()
-    cached_embeddings = CacheBackedEmbeddings.from_bytes_store(embeddings, cache_dir)
-    vectorstore = FAISS.from_documents(docs, cached_embeddings)
-    retriever = vectorstore.as_retriever()
-    return retriever
-
-
-if "messages" not in st.session_state:
-    st.session_state["messages"] = []
-
-
-def save_message(message, role):
-    st.session_state["messages"].append({"message": message, "role": role})
-
-
-def send_message(message, role, save=True):
-    with st.chat_message(role):
-        st.markdown(message)
-    if save:
-        save_message(message, role)
-
-
-def paint_history():
-    for message in st.session_state["messages"]:
-        send_message(
-            message["message"],
-            message["role"],
-            save=False,
-        )
-
-
-def format_docs(docs):
-    return "\n\n".join(document.page_content for document in docs)
-
 
 prompt = ChatPromptTemplate.from_messages(
     [
@@ -121,15 +67,77 @@ prompt = ChatPromptTemplate.from_messages(
     ]
 )
 
+
+@st.cache_resource(show_spinner="Embedding the file...")
+def embed_file(file):
+
+    file_content = file.read()
+
+    os.makedirs(f"./.cache/files/", exist_ok=True)
+    file_path = f"./.cache/files/{file.name}"
+
+    with open(file_path, "wb") as f:
+        f.write(file_content)
+
+    os.makedirs(f"./.cache/embeddings", exist_ok=True)
+    cache_dir = LocalFileStore(f"./.cache/embeddings/{file.name}")
+
+    splitter = CharacterTextSplitter.from_tiktoken_encoder(
+        separator="\n",
+        chunk_size=600,
+        chunk_overlap=100,
+    )
+
+    loader = UnstructuredFileLoader(file_path)
+
+    docs = loader.load_and_split(text_splitter=splitter)
+
+    embeddings = OpenAIEmbeddings()
+
+    cached_embeddings = CacheBackedEmbeddings.from_bytes_store(embeddings, cache_dir)
+
+    vectorstore = FAISS.from_documents(docs, cached_embeddings)
+
+    retriever = vectorstore.as_retriever()
+
+    return retriever
+
+
+def save_message(message, role):
+    st.session_state["messages"].append({"message": message, "role": role})
+
+
+def save_api_key(api_key):
+    st.session_state["api_key"] = api_key
+    st.session_state["api_key_bool"] = True
+
+
+def send_message(message, role, save=True):
+    with st.chat_message(role):
+        st.markdown(message)
+
+    if save:
+        save_message(message, role)
+
+
+def paint_history():
+    for message in st.session_state["messages"]:
+        send_message(message["message"], message["role"], save=False)
+
+
+def format_docs(docs):
+    return "\n\n".join(document.page_content for document in docs)
+
+
 st.title("DocumentGPT")
 
 st.markdown(
     """
 Welcome!
-            
+
 Use this chatbot to ask questions to an AI about your files!
 
-Upload your files on the sidebar.
+Upload file on the sidebar to get started.
 """
 )
 
@@ -138,39 +146,51 @@ with st.sidebar:
         "Upload a .txt .pdf or .docx file",
         type=["txt", "pdf", "docx"],
     )
+
+with st.sidebar:
     api_key = st.text_input(
-        "Please Enter your 'OpenAI API_KEY'",
-        value=st.session_state.get("api_key", ""),
-        key="api_key_input",
-    )
-    if st.button("Clear API Key"):
-        st.session_state["api_key"] = ""
-        st.experimental_rerun()
+        "Please Enter your OpenAI_API_KEY.",
+        disabled=st.session_state["api_key"] is not None,
+    ).strip()
+
+    if api_key:
+        save_api_key(api_key)
+        st.write("OpenAI_API_KEY has been saved.")
+
+    button = st.button("Save")
+
+    if button:
+        save_api_key(api_key)
+        if api_key == "":
+            st.write("Please Enter your OpenAI_API_KEY.")
+
     st.markdown(
         "[GitHub Repository Link](https://github.com/Ehbebe/fullstack-gpt/blob/main/pages/01_DocumentGPT.py)"
     )
 
-if not file or not api_key:
-    st.warning("Please upload a file and provide an OpenAI API Key to proceed.")
-    st.stop()
+if (st.session_state["api_key_bool"] == True) and (st.session_state["api_key"] != None):
+    if file:
+        retriever = embed_file(file)
+        send_message("I'm ready! Ask away!", "ai", save=False)
+        paint_history()
+        message = st.chat_input("Ask anything about your file")
 
-if file:
-    retriever = embed_file(file)
-    send_message("I'm ready! Ask away!", "ai", save=False)
-    paint_history()
-    message = st.chat_input("Ask anything about your file...")
-    if message:
-        send_message(message, "human")
-        chain = (
-            {
-                "context": retriever | RunnableLambda(format_docs),
-                "question": RunnablePassthrough(),
-            }
-            | prompt
-            | llm
-        )
-        with st.chat_message("ai"):
-            chain.invoke(message)
+        if message:
+            if re.match(pattern, st.session_state["api_key"]):
+                send_message(message, "human")
+                chain = (
+                    {
+                        "context": retriever | RunnableLambda(format_docs),
+                        "question": RunnablePassthrough(),
+                    }
+                    | prompt
+                    | llm
+                )
 
-else:
-    st.session_state["messages"] = []
+                with st.chat_message("ai"):
+                    resposne = chain.invoke(message)
+            else:
+                message = "OPENAI_API_KEY is invalid. Please reinsert it."
+                send_message(message, "ai")
+    else:
+        st.session_state["messages"] = []
