0f81917e68f1b9e18454450dc196328a7178baf1
Ehbebe <study2hojin@gmail.com>
2024-04-09 22:56:11 +0900

    QuizGPT & SiteGPT upload

---
+++ Home.py
@@ -0,0 +1,20 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Streamlit Challenge Home",
+    page_icon=":tada:",
+)
+
+st.markdown(
+    """
+# Hello!
+            
+Welcome to my Streamlit Challenge!
+            
+Here are the apps I made:
+            
+- [x] [DocumentGPT](/DocumentGPT)
+- [x] [QuizGPT](/QuizGPT)
+- [x] [SiteGPT](/SiteGPT)
+"""
+)
